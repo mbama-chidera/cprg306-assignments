ea1804cbe09c1ec8782f6c5648a6f755e3fd2016
Mbama Chidera <chidera.mbama@edu.sait.ca>
2025-09-20 20:56:08 -0600

    fix

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function StudentInfo() {
   return (
     <section>
-      <p>Student: Mbama Chidera</p>
+      <p>Mbama Chidera</p>
       <p>
-        Repository:{" "}
+
         <Link href="https://github.com/mbama-chidera/cprg306-assignments">
           https://github.com/mbama-chidera/cprg306-assignments
         </Link>
