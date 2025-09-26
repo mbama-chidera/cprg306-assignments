81a99e59b92eba64532d7bcf93a7fc873a09dea8
Mbama Chidera <chidera.mbama@edu.sait.ca>
2025-09-26 17:24:19 -0600

    change

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -1,92 +1,20 @@
 import Item from "./item";
 
-const item1 = {
-  name: "milk, 4 L 🥛",
-  quantity: 1,
-  category: "dairy",
-};
-
-const item2 = {
-  name: "bread 🍞",
-  quantity: 2,
-  category: "bakery",
-};
-
-const item3 = {
-  name: "eggs, dozen 🥚",
-  quantity: 2,
-  category: "dairy",
-};
-
-const item4 = {
-  name: "bananas 🍌",
-  quantity: 6,
-  category: "produce",
-};
-
-const item5 = {
-  name: "broccoli 🥦",
-  quantity: 3,
-  category: "produce",
-};
-
-const item6 = {
-  name: "chicken breasts, 1 kg 🍗",
-  quantity: 1,
-  category: "meat",
-};
-
-const item7 = {
-  name: "pasta sauce 🍝",
-  quantity: 3,
-  category: "canned goods",
-};
-
-const item8 = {
-  name: "spaghetti, 454 g 🍝",
-  quantity: 2,
-  category: "dry goods",
-};
-
-const item9 = {
-  name: "toilet paper, 12 pack 🧻",
-  quantity: 1,
-  category: "household",
-};
-
-const item10 = {
-  name: "paper towels, 6 pack",
-  quantity: 1,
-  category: "household",
-};
-
-const item11 = {
-  name: "dish soap 🍽️",
-  quantity: 1,
-  category: "household",
-};
-
-const item12 = {
-  name: "hand soap 🧼",
-  quantity: 4,
-  category: "household",
-};
-
 export default function ItemList() {
   return (
     <ul role="list" className="space-y-4">
-      <Item {...item1} />
-      <Item {...item2} />
-      <Item {...item3} />
-      <Item {...item4} />
-      <Item {...item5} />
-      <Item {...item6} />
-      <Item {...item7} />
-      <Item {...item8} />
-      <Item {...item9} />
-      <Item {...item10} />
-      <Item {...item11} />
-      <Item {...item12} />
+      <Item name="milk, 4 L 🥛" quantity={1} category="dairy" />
+      <Item name="bread 🍞" quantity={2} category="bakery" />
+      <Item name="eggs, dozen 🥚" quantity={2} category="dairy" />
+      <Item name="bananas 🍌" quantity={6} category="produce" />
+      <Item name="broccoli 🥦" quantity={3} category="produce" />
+      <Item name="chicken breasts, 1 kg 🍗" quantity={1} category="meat" />
+      <Item name="pasta sauce 🍝" quantity={3} category="canned goods" />
+      <Item name="spaghetti, 454 g 🍝" quantity={2} category="dry goods" />
+      <Item name="toilet paper, 12 pack 🧻" quantity={1} category="household" />
+      <Item name="paper towels, 6 pack" quantity={1} category="household" />
+      <Item name="dish soap 🍽️" quantity={1} category="household" />
+      <Item name="hand soap 🧼" quantity={4} category="household" />
     </ul>
   );
 }
