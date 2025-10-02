32e34179cb54a2adfcfa7f4c6bbc7b0dadde6a54
Mbama Chidera <chidera.mbama@edu.sait.ca>
2025-10-02 13:24:40 -0600

    fix week 3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,8 +2,8 @@
 export default function Item({ name, quantity, category }) {
   return (
     <li className="bg-slate-800 my-4 p-2 w-96">
-      <p className="text-2xl font-bold text-white">{name}</p>
-      <p className="text-lg font-bold">
+      <p className="text-2xl font-bold text-green-600">{name}</p>
+      <p className="text-lg font-bold text-green-600">
         Buy {quantity} in {category}
       </p>
     </li>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <main className="mx-auto max-w-md p-4">
-      <h1 className="text-4xl font-bold text-white">
+      <h1 className="text-4xl font-bold text-green-600 mb-4">
         Shopping List
       </h1>
       <ItemList />
